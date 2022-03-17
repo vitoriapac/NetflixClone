f554b7675e223babbefdac92cee06cc61e24da9c
Vitoria Pacheco <vitoriac.pac@gmail.com>
2022-03-17 16:04:07 -0300

    list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import Tmdb from './tmdb'
+import MovieRow from './Components/MovieRow'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
+  const [movieList, setMovieList] = useState([])
+
+  useEffect(() => {
+    const loadAll = async () => {
+      //Pegar lista totalament
+      let list = await Tmdb.getHomeList()
+      setMovieList(list)
+    }
+
+    loadAll()
+  }, [])
+
   return (
-    <div>
-      <h1>Ola Mundo!</h1>
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
     </div>
   )
 }
