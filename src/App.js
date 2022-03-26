44854c436f59b193ff7804b53caafc195c58c05f
Vitoria Pacheco <vitoriac.pac@gmail.com>
2022-03-26 19:59:14 -0300

    Ajustes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React, { useEffect, useState } from 'react'
 import './App.css'
 import Tmdb from './tmdb'
 import MovieRow from './Components/MovieRow'
+import FeaturedMovie from './Components/FeaturedMovie'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
   const [movieList, setMovieList] = useState([])
+  const [featuredData, setFeaturedData] = useState(null)
 
   useEffect(() => {
     const loadAll = async () => {
       //Pegar lista totalament
       let list = await Tmdb.getHomeList()
       setMovieList(list)
+
+      //Pegar lista em destaque
     }
 
     loadAll()
@@ -19,6 +23,8 @@ export default () => {
 
   return (
     <div className="page">
+      {featuredData && <FeaturedMovie item={featuredData} />}
+
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
